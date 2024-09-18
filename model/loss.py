28af309cdb9ff690bb109ebdddbc02af0ac7f920
frogbam <frogbam07@gmail.com>
2024-09-18 22:23:05 +0900

    예시용 config와 loss 추가

--- model/loss.py
+++ model/loss.py
@@ -16,3 +16,20 @@ class CELoss(nn.Module):
     ) -> torch.Tensor:
     
         return self.loss_fn(outputs, targets)
+    
+
+class SmoothingLoss(nn.Module):
+    """
+    모델의 손실함수를 계산하는 클래스.
+    """
+    def __init__(self):
+        super(SmoothingLoss, self).__init__()
+        self.loss_fn = nn.CrossEntropyLoss(label_smoothing=0.1)
+
+    def forward(
+        self,
+        outputs: torch.Tensor,
+        targets: torch.Tensor
+    ) -> torch.Tensor:
+
+        return self.loss_fn(outputs, targets)
